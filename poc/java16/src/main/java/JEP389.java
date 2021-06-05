e0fe123665a9f6adc23bf5e007bbd04b683c844c
Ivo Woltring <ivo@ivonet.nl>
2021-06-05 20:05:33 +0200

    Added Java 16 article

--- poc/java16/src/main/java/JEP389.java
+++ poc/java16/src/main/java/JEP389.java
@@ -7,11 +7,13 @@ import java.nio.file.Path;
 
 public class JEP389 {
   public static void main(final String[] args) throws Throwable {
-    var lib = LibraryLookup.ofPath(Path.of("/src/helloworld.so"));
+    var lib = LibraryLookup
+          .ofPath(Path.of("/src/helloworld.so"));
     var sym = lib.lookup("helloworld").get();
     var fd = FunctionDescriptor.ofVoid();
     var mt = MethodType.methodType(Void.TYPE);
-    var mh = CLinker.getInstance().downcallHandle(sym.address(), mt, fd);
+    var mh = CLinker.getInstance()
+                    .downcallHandle(sym.address(), mt, fd);
     mh.invokeExact();
   }
 }
--- poc/java16/src/main/java/JEP394.java
+++ poc/java16/src/main/java/JEP394.java
@@ -1,7 +1,8 @@
 public class JEP394 {
 
     public void print(Object o) {
-        if (o instanceof String bs && bs.length() > 5){
+        if (o instanceof String bs &&
+              bs.length() > 5){
             System.out.println(bs);
         } else {
             System.out.println("Shorter");
